3d3a018e70f5b61b0fa3e5c05d1b752850f01d30
Yanggoo <52310349+Yanggoo@users.noreply.github.com>
2024-04-16 20:20:14 +0800

    Remove a submodule.

--- src/Hazel.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-//for use by Hazel applications
-#include "Hazel/Application.h"
-//---Entry Point----------------- 
-#include "Hazel/EntryPoint.h"
-//---Entry Point----------------- 
